75204953bc0b83e225a93534d7b5cf83568da7ec
Ayesha Padrawala <apadrawala5@gmail.com>
2024-01-16 11:08:19 +0530

    Adding space identation

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,65 +1,69 @@
 # app/controllers/api/v1/employees_controller.rb
 module Api
-    module V1
-      class EmployeesController < ApplicationController
+  module V1
+    class EmployeesController < ApplicationController
             # before_action :authenticate_user!
-            before_action :authenticate_admin, only: [:create, :index, :show, :update, :destroy]
+        before_action :authenticate_admin, only: [:create, :index, :show, :update, :destroy]
 
-            def index
-                @employees = Employee.all
-                render json: {data: @employees, message: "Employees are fetched successfully"}, status: :ok 
-            end
-            def show 
-                @employee = Employee.find(params[:id])
-                render json: {data: @employee, message: "Employee is fetched successfully"}, status: :ok
-            end
-            def create 
-                @employee = Employee.new(emp_params)
-                @employee.user = current_user
-                if @employee.save
-                    render json: {data: @employee, message: "Employee created succesfully"}, status: :created
-                else
-                    render json: {error: @employee.errors.full_messages}, status: :unprocessable_entity
-                end
-            end
-            def update 
-                @employee = Employee.find_by_id(params[:id])
-                if @employee.update(emp_params)
-                    render json: {data: @employee, message: "Employee updated successfully"}, status: :ok
-                else
-                    render json: {error: @employee.errors.full_messages}, status: :unprocessable_entity
-                end
-            end
-            def destroy
-                @employee = Employee.find_by_id(params[:id])
-                @employee.destroy
-                head :no_content
-            end
-
-            private
-
-            def emp_params
-                params.require(:employee).permit(:name, :email)
-            end
-
-            def authenticate_admin
-                # Use Doorkeeper's current_resource_owner
-                user = current_user
-                # Modify the condition to check if the user is an admin
-                unless user&.admin?
-                  # No need to sign in again, as Doorkeeper will handle it
-                  render json: { error: "Unauthorized access" }, status: :unauthorized
-                end
-            end
-            # def authenticate_admin
-            #     user = User.find_by(email: params[:email])
-            #     if user&.admin? && user.valid_password?(params[:password])
-            #         sign_in(user, store: false)
-            #     else
-            #         render json:{error: "Unauthorized access"}, status: :unauthorized
-            #     end
-            # end
+        def index
+          @employees = Employee.all
+          render json: {data: @employees, message: "Employees are fetched successfully"}, status: :ok 
         end
-    end
+
+        def show 
+          @employee = Employee.find(params[:id])
+          render json: {data: @employee, message: "Employee is fetched successfully"}, status: :ok
+        end
+
+        def create 
+          @employee = Employee.new(emp_params)
+          @employee.user = current_user
+          if @employee.save
+              render json: {data: @employee, message: "Employee created succesfully"}, status: :created
+          else
+              render json: {error: @employee.errors.full_messages}, status: :unprocessable_entity
+          end
+        end
+
+        def update 
+          @employee = Employee.find_by_id(params[:id])
+          if @employee.update(emp_params)
+              render json: {data: @employee, message: "Employee updated successfully"}, status: :ok
+          else
+              render json: {error: @employee.errors.full_messages}, status: :unprocessable_entity
+          end
+        end
+
+        def destroy
+          @employee = Employee.find_by_id(params[:id])
+          @employee.destroy
+          head :no_content
+        end
+
+        private
+
+          def emp_params
+            params.require(:employee).permit(:name, :email)
+          end
+
+          def authenticate_admin
+                  # Use Doorkeeper's current_resource_owner
+            user = current_user
+                  # Modify the condition to check if the user is an admin
+            unless user&.admin?
+                    # No need to sign in again, as Doorkeeper will handle it
+            render json: { error: "Unauthorized access" }, status: :unauthorized
+            end
+          end
+              # def authenticate_admin
+              #     user = User.find_by(email: params[:email])
+              #     if user&.admin? && user.valid_password?(params[:password])
+              #         sign_in(user, store: false)
+              #     else
+              #         render json:{error: "Unauthorized access"}, status: :unauthorized
+              #     end
+              # end
+      end
+  end
 end
   
--- app/controllers/api/v1/holidays_controller.rb
+++ app/controllers/api/v1/holidays_controller.rb
@@ -1,45 +1,43 @@
 module Api
-    module V1
+  module V1
+    class HolidaysController < ApplicationController
+        
+      def index
+         @holidays = Holiday.all
+        render json: {data: @holiday, message: "Holidays are fetched successfully"}, status: :ok
+      end
 
-        class HolidaysController < ApplicationController
+      def show
+        @holiday = Holiday.find_by_id(params[:id])
+        render json: {data: @holiday, message: "Holiday is fetched successfully"}, status: :ok
+      end
+          
+      def index_for_employee
+        @employee = current_user.employees.find(params[:employee_id])
+        @holiday = @employee.holidays
+        if @holiday
+          render json: {data: @holiday , nessage: "Holidays for specific employee"}, status: :ok
+        else
+            render json: {error: @holiday.errors.full_messages}, status: :unprocessable_entity
+        end      
+      end
 
-            def index
-                @holidays = Holiday.all
-                render json: {data: @holiday, message: "Holidays are fetched successfully"}, status: :ok
-            end
-
-            def show
-                @holiday = Holiday.find_by_id(params[:id])
-                render json: {data: @holiday, message: "Holiday is fetched successfully"}, status: :ok
-            end
-            
-            def index_for_employee
-                @employee = current_user.employees.find(params[:employee_id])
-                @holiday = @employee.holidays
-                if @holiday
-                  render json: {data: @holiday , nessage: "Holidays for specific employee"}, status: :ok
-                else
-                    render json: {error: @holiday.errors.full_messages}, status: :unprocessable_entity
-                end
-            end
-
-            def create
-                @employee = current_user.employees.find(holiday_params[:employee_id])
-                @holiday = @employee.holidays.new(holiday_params)
-                if @holiday.save
-                    render json: {data: @holiday, message: "Holiday is created successfully"}, status: :created
-                else
-                    render json: {error: @holiday.errors.full_messages}, status: :unprocessable_entity
-                end
-            end
-
-            private
-
-            def holiday_params
-                params.require(:holiday).permit(:h_type, :description, :start_date, :end_date, :employee_id)
-            end
-
+      def create
+        @employee = current_user.employees.find(holiday_params[:employee_id])
+        @holiday = @employee.holidays.new(holiday_params)
+        if @holiday.save
+            render json: {data: @holiday, message: "Holiday is created successfully"}, status: :created
+        else
+            render json: {error: @holiday.errors.full_messages}, status: :unprocessable_entity
         end
-    end
+      end
 
+      private
+
+      def holiday_params
+        params.require(:holiday).permit(:h_type, :description, :start_date, :end_date, :employee_id)
+      end
+
+    end
+  end
 end
