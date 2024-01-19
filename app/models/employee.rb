efd966570ff034952716f49256918b3d5fb2aefb
Ayesha Padrawala <apadrawala5@gmail.com>
2024-01-19 10:41:32 +0530

    No changes

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,6 @@
 class Employee < ApplicationRecord
     belongs_to :user
     has_many :holidays
-
     def self.authenticate(email, password)
         employee = Employee.find_by(email: email)
         employee&.valid_password?(password) ? employee : nil
